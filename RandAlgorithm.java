927a7afc72b906cec9e7f9123765ef3d5cb22eaf
1kimjungbin <2020284@sungil-i.kr>
2021-06-17 08:40:31 +0900

    6월 17일 수업

--- RandAlgorithm.java
+++ RandAlgorithm.java
@@ -4,7 +4,7 @@ public class RandAlgorithm {
     public static void main(String arg[]) {
         Rand myRand = new Rand();
         //저장할 멤버변수 설정 (배열 형식)
-    int[] input = {0,};
+    int[] input = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     //랜덤 변수 만들기
     Random rand = new Random();
@@ -12,10 +12,16 @@ public class RandAlgorithm {
         input[i] = (rand.nextInt(10) + 1);
         System.out.print(input[i] + " ");
     }
-    System.out.println("1부터 10사이의 랜덤변수" + (rand.nextInt(10) + 1)
+    System.out.println("1부터 10사이의 랜덤변수 : " + (rand.nextInt(10) + 1)
     );
 
     //랜덤 변수에서 최소값 구하기
+    int minValue = 999999;
+    for(int i = 0; i<10; i++){
+        if(input[i] < minValue){
+            minValue = input[i];
+        }
+    }
     }
     
 }
